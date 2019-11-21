4d0ae3e1bbb8563c2baac44d72bf3c040a12a2b5
Paweł Grzegorzewski <pawelgr@gmail.com>
2019-11-21 07:26:16 +0100

    Move scan code to separate class

--- src/CameraLongRangeScanner/Program.cs
+++ src/CameraLongRangeScanner/Program.cs
@@ -2,6 +2,8 @@
 using Sandbox.ModAPI.Interfaces;
 using SpaceEngineers.Game.ModAPI.Ingame;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using VRageMath;
 
@@ -17,7 +19,7 @@ namespace ScriptingClass
 		//	Me.CustomData = strPBGPS;
 		//}
 
-
+		PinpointScanSystem system;
 
 		public Program()
 
@@ -35,10 +37,10 @@ namespace ScriptingClass
 			// timer block.
 
 			// Configure this program to run the Main method every 100 update ticks
-			Runtime.UpdateFrequency = UpdateFrequency.Update100;
+			//Runtime.UpdateFrequency = UpdateFrequency.Update100;
+
+			system = new PinpointScanSystem(this);
 		}
-
-
 
 		public void Save()
 
@@ -52,7 +54,6 @@ namespace ScriptingClass
 			// needed.
 
 		}
-
 
 		// Camera Scan script
 		// BY DerLaCroix  
@@ -68,240 +69,285 @@ namespace ScriptingClass
 		//Optional:
 		//Second Timer block to be triggererd on detection - specify name in variable TIMERBLOCKTRIGGER
 
-		//Name of camera block
-		String SCAN_CAMERA = "SCAN_CAMERA";
+		//New wey to setup
+		//Add all cammeras and lcds you wont to use to group with name "ScanSystem"
 
-		//Name of lcd panel
-		String SCAN_INFO = "SCAN_INFO";
+		public void Main(string argument, UpdateType updateSource)
 
-		String SCAN_INFO2 = "SCAN_INFO_2";
-
-		//Trigger block on detection
-		String TIMERBLOCKTRIGGER = string.Empty;
-
-		double RANGE = 20000;
-		float PITCH = 0;
-		float YAW = 0;
-		private IMyCameraBlock camera;
-		private IMyTextPanel lcd;
-		private IMyTextPanel lcd2;
-		private IMyTimerBlock trigger;
-		private bool firstrun = true;
-		private MyDetectedEntityInfo info;
-		private StringBuilder sb = new StringBuilder();
-		private bool triggerBlockOnContact = false;
-
-		void Main(String setRange)
 		{
 
-			if (!string.IsNullOrEmpty(setRange))
-			{
-				RANGE = Convert.ToDouble(setRange.Trim());
-			}
-			if (firstrun)
-			{
-				firstrun = false;
-				Echo("Initializing System, detecting requirements.");
-				Echo("Searching camera...");
-				camera = GridTerminalSystem.GetBlockWithName(SCAN_CAMERA) as IMyCameraBlock;
-				if (camera != null)
-				{
-					Echo($"Camera {camera.CustomName} found.");
-				}
-				else
-				{
-					Echo($"Camera {SCAN_CAMERA} not found.");
-				}
-				Echo("Searching output lcd panel...");
-				lcd = GridTerminalSystem.GetBlockWithName(SCAN_INFO) as IMyTextPanel;
-				if (lcd != null)
-				{
-					Echo($"lcd {camera.CustomName} found.");
-				}
-				else
-				{
-					Echo($"lcd {SCAN_INFO} not found.");
-				}
+			// The main entry point of the script, invoked every time
+			// one of the programmable block's Run actions are invoked,
+			// or the script updates itself. The updateSource argument
+			// describes where the update came from.
+			// 
+			// The method itself is required, but the arguments above
+			// can be removed if not needed.
 
-				lcd2 = GridTerminalSystem.GetBlockWithName(SCAN_INFO2) as IMyTextPanel;
-				if (lcd != null)
+			if (string.IsNullOrEmpty(argument))
+			{
+				system.DoScan();
+			}
+
+			system.SetRange(argument);
+
+		}
+
+		public class PinpointScanSystem
+		{
+
+			//New wey to setup
+			//Add all cammeras and lcds you wont to use to group with name "ScanSystem"
+
+			const string SCAN_SYSTEM_GROUP = "ScanSystem";
+
+			//Trigger block on detection
+			String TIMERBLOCKTRIGGER = string.Empty;
+
+			double RANGE = 20000;
+			float PITCH = 0;
+			float YAW = 0;
+			//private IMyCameraBlock camera;
+			//private IMyTextPanel lcd;
+			//private IMyTextPanel lcd2;
+			private IMyTimerBlock trigger;
+			//private bool firstrun = true;
+			//private MyDetectedEntityInfo info;
+			//private StringBuilder sb = new StringBuilder();
+			private bool triggerBlockOnContact = false;
+
+
+			private Program _program;
+			private List<IMyCameraBlock> cameras;
+			private List<IMyTextPanel> displays;
+
+			List<MyDetectedEntityInfo> detectedList;
+
+			public PinpointScanSystem(Program program)
+			{
+				_program = program;
+
+				detectedList = new List<MyDetectedEntityInfo>();
+
+				IMyBlockGroup scanSystemGroup = _program.GridTerminalSystem.GetBlockGroupWithName(SCAN_SYSTEM_GROUP);
+				if (scanSystemGroup == null)
 				{
-					Echo($"lcd {camera.CustomName} found.");
+					throw new Exception($"No {SCAN_SYSTEM_GROUP} group defined.");
 				}
-				else
+				cameras = new List<IMyCameraBlock>();
+				scanSystemGroup.GetBlocksOfType<IMyCameraBlock>(cameras);
+				if (cameras.Count == 0)
 				{
-					Echo($"lcd {SCAN_INFO} not found.");
+					throw new Exception("No camera found in group.");
+				}
+				displays = new List<IMyTextPanel>();
+				scanSystemGroup.GetBlocksOfType<IMyTextPanel>(displays);
+				if (cameras.Count == 0)
+				{
+					throw new Exception("No text pannel found in group.");
 				}
 
 				if (TIMERBLOCKTRIGGER != string.Empty)
 				{
 					//we are to trigger a block on contact
-					Echo("Searching camera...");
-					trigger = GridTerminalSystem.GetBlockWithName(TIMERBLOCKTRIGGER) as IMyTimerBlock;
+					_program.Echo("Searching timmer block...");
+					trigger = _program.GridTerminalSystem.GetBlockWithName(TIMERBLOCKTRIGGER) as IMyTimerBlock;
 					if (trigger != null)
 					{
-						Echo($"Timer {trigger.CustomName} found.");
+						_program.Echo($"Timer {trigger.CustomName} found.");
 						triggerBlockOnContact = true;
 					}
 					else
 					{
-						Echo($"Timer {TIMERBLOCKTRIGGER} not found.");
+						_program.Echo($"Timer {TIMERBLOCKTRIGGER} not found.");
 					}
 
 				}
-				camera.EnableRaycast = true;
-			}
 
-			sb.Clear();
-			if (camera.CanScan(RANGE))
-			{
-
-				info = camera.Raycast(RANGE, PITCH, YAW);
-				sb.Append($"Last scan at range: {RANGE}");
-			}
-			else
-			{
-
-
-				sb.Append($"Powering up for scan... Current range: {camera.AvailableScanRange}");
+				//Enable raycast for cameras
+				foreach (var camera in cameras)
+				{
+					camera.EnableRaycast = true;
+				}
 
 			}
 
-
-			if (info.EntityId == 0)
+			public void SetRange(string setRangeValue)
 			{
-				//nothing found
-				sb.AppendLine();
+				if (!string.IsNullOrEmpty(setRangeValue))
+				{
+					RANGE = Convert.ToDouble(setRangeValue.Trim());
+				}
+			}
+			public void DoScan()
+			{
+
+				_program.Echo("Prepare scan");
+				//sb.Clear();
+
+
+
+				foreach (var camera in cameras)
+				{
+					var info = camera.Raycast(RANGE, PITCH, YAW);
+					if (!info.IsEmpty())
+					{
+						if (!detectedList.Exists(x => x.EntityId == info.EntityId))
+						{
+
+							DisplayNonEmptyInfo(info, camera);
+							detectedList.Add(info);
+						}
+					}
+					DisplayEmptyInfo(info);
+				}
+			}
+
+			private void DisplayEmptyInfo(MyDetectedEntityInfo info)
+			{
+				var sb = new StringBuilder();
 				sb.Append($"No target found within  {RANGE} m");
-				lcd.SetValue("FontColor", Color.White);
-				lcd.WritePublicText(sb.ToString());
-				lcd.ShowPublicTextOnScreen();
+				var firstDisplay = displays.OrderBy(x => x.DisplayNameText).FirstOrDefault();
+				//firstDisplay.SetValue("FontColor", Color.White);
+				//firstDisplay.WritePublicText(sb.ToString());
+				//firstDisplay.ShowPublicTextOnScreen();
 				return;
 			}
-			if (info.HitPosition.HasValue)
+
+			private void DisplayNonEmptyInfo(MyDetectedEntityInfo info, IMyCameraBlock camera)
 			{
-				if (triggerBlockOnContact)
-				{
-					trigger.Trigger();
-				}
-				sb.AppendLine();
-				sb.Append($"Target found at {(Vector3D.Distance(camera.GetPosition(), info.HitPosition.Value)):0.00}m range");
-			}
+				var sb = new StringBuilder();
 
-
-			String size = info.BoundingBox.Size.ToString("0.000");
-			String printSize = string.Empty;
-			double volume = 1;
-			String unitName = "m3";
-			if (size != null || size != string.Empty)
-			{
-				size = size.Replace("{", string.Empty).Replace("}", string.Empty).Replace("X", string.Empty).Replace("Y", string.Empty).Replace("Z", string.Empty).Replace(" ", string.Empty);
-				size = size.Trim();
-				String[] sizes = size.Split(':');
-
-				double factor = 1;
-
-				if (info.Type.ToString() == "SmallGrid")
+				if (info.HitPosition.HasValue)
 				{
-					factor = 4;
-					unitName = "blocks";
-				}
-				else if (info.Type.ToString() == "LargeGrid")
-				{
-					factor = 16;
-					unitName = "blocks";
-				}
-				foreach (String measure in sizes)
-				{
-					if (measure == string.Empty)
+					if (triggerBlockOnContact)
 					{
-						//first value
-						continue;
+						trigger.Trigger();
+					}
+					sb.AppendLine();
+					sb.AppendLine($"Time: {DateTime.Now.ToString()}");
+					sb.Append($"Target found at {(Vector3D.Distance(camera.GetPosition(), info.HitPosition.Value)):0.00}m range");
+
+					String size = info.BoundingBox.Size.ToString("0.000");
+					String printSize = string.Empty;
+					double volume = 1;
+					String unitName = "m3";
+					if (size != null || size != string.Empty)
+					{
+						size = size.Replace("{", string.Empty).Replace("}", string.Empty).Replace("X", string.Empty).Replace("Y", string.Empty).Replace("Z", string.Empty).Replace(" ", string.Empty);
+						size = size.Trim();
+						String[] sizes = size.Split(':');
+
+						double factor = 1;
+
+						if (info.Type.ToString() == "SmallGrid")
+						{
+							factor = 4;
+							unitName = "blocks";
+						}
+						else if (info.Type.ToString() == "LargeGrid")
+						{
+							factor = 16;
+							unitName = "blocks";
+						}
+						foreach (String measure in sizes)
+						{
+							if (measure == string.Empty)
+							{
+								//first value
+								continue;
+							}
+
+							volume = volume * Math.Round(Convert.ToDouble(measure), 0);
+
+							printSize += $"{Convert.ToDouble(measure):0.00} X ";
+						}
+						volume = volume / factor;
+					}
+					printSize = printSize.Remove(printSize.Length - 2);
+					printSize += "m";
+
+					//X:5.302 Y:3.072 Z:4.252
+
+					sb.AppendLine();
+					sb.Append($"Found grid ID: {info.EntityId}");
+					sb.AppendLine();
+					sb.Append($"Name of object: {info.Name}");
+					sb.AppendLine();
+					sb.Append($"Type of object: {info.Type}");
+					sb.AppendLine();
+					sb.Append($"Current velocity: {info.Velocity.ToString("0.000")}");
+					sb.AppendLine();
+					sb.Append($"Relationship: {info.Relationship}");
+					sb.AppendLine();
+					sb.Append($"Size: {printSize}");
+					sb.AppendLine();
+					sb.Append($"Estimated volume: {volume} {unitName}");
+					sb.AppendLine();
+					sb.Append($"Position: {info.Position.ToString("0.000")}");
+					sb.AppendLine();
+					sb.Append($"GPS:object{(info.Position.ToString("0.000").Replace("{", string.Empty).Replace("}", string.Empty).Replace("X", string.Empty).Replace("Y", string.Empty).Replace("Z", string.Empty).Replace(" ", string.Empty))}:");
+
+					Color color = Color.White;
+
+					if (info.Relationship.ToString() == "Owner")
+					{
+						color = Color.Green;
+					}
+					else if (info.Relationship.ToString() == "Friendly")
+					{
+						color = Color.Green;
+					}
+					else if (info.Relationship.ToString() == "FactionShare")
+					{
+						color = Color.Green;
+					}
+					else if (info.Relationship.ToString() == "Floating")
+					{
+						color = Color.Yellow;
+					}
+					else if (info.Relationship.ToString() == "NoOwnership")
+					{
+						color = Color.Yellow;
+					}
+					else if (info.Relationship.ToString() == "Enemies")
+					{
+						color = Color.Red;
 					}
 
-					volume = volume * Math.Round(Convert.ToDouble(measure), 0);
+					if (info.HitPosition.HasValue)
+					{
+						sb.AppendLine();
+						sb.Append($"Hit point: {info.HitPosition.Value.ToString("0.000")}");
+						sb.AppendLine();
+						sb.Append($"GPS:objectHit{(info.HitPosition.Value.ToString("0.000").Replace("{", string.Empty).Replace("}", string.Empty).Replace("X", string.Empty).Replace("Y", string.Empty).Replace("Z", string.Empty).Replace(" ", string.Empty))}:");
+					}
 
-					printSize += $"{Convert.ToDouble(measure):0.00} X ";
+					DisplayNonEmptyInfo(sb.ToString(), color);
 				}
-				volume = volume / factor;
 			}
-			printSize = printSize.Remove(printSize.Length - 2);
-			printSize += "m";
 
-			//X:5.302 Y:3.072 Z:4.252
-
-			sb.AppendLine();
-			sb.Append($"Found grid ID: {info.EntityId}");
-			sb.AppendLine();
-			sb.Append($"Name of object: {info.Name}");
-			sb.AppendLine();
-			sb.Append($"Type of object: {info.Type}");
-			sb.AppendLine();
-			sb.Append($"Current velocity: {info.Velocity.ToString("0.000")}");
-			sb.AppendLine();
-			sb.Append($"Relationship: {info.Relationship}");
-			sb.AppendLine();
-			sb.Append($"Size: {printSize}");
-			sb.AppendLine();
-			sb.Append($"Estimated volume: {volume} {unitName}");
-			sb.AppendLine();
-			sb.Append($"Position: {info.Position.ToString("0.000")}");
-			sb.AppendLine();
-			sb.Append($"GPS:object{(info.Position.ToString("0.000").Replace("{", string.Empty).Replace("}", string.Empty).Replace("X", string.Empty).Replace("Y", string.Empty).Replace("Z", string.Empty).Replace(" ", string.Empty))}:");
-
-			Color color = Color.White;
-
-			if (info.Relationship.ToString() == "Owner")
+			private void DisplayNonEmptyInfo(string text, Color color)
 			{
-				color = Color.Green;
+				var displaysOrdered = displays.OrderBy(x => x.DisplayNameText).ToList();
+
+				for (int i = displaysOrdered.Count - 1; i > 0; i--)
+				{
+
+					DisplayNonEmptyInfo(displaysOrdered[i], displaysOrdered[i - 1].GetPublicText(), displaysOrdered[i - 1].GetValueColor("FontColor"));
+				}
+				DisplayNonEmptyInfo(displaysOrdered[0], text, color);
 			}
-			else if (info.Relationship.ToString() == "Friendly")
+			private void DisplayNonEmptyInfo(IMyTextPanel display, string text, Color color)
 			{
-				color = Color.Green;
-			}
-			else if (info.Relationship.ToString() == "FactionShare")
-			{
-				color = Color.Green;
-			}
-			else if (info.Relationship.ToString() == "Floating")
-			{
-				color = Color.Yellow;
-			}
-			else if (info.Relationship.ToString() == "NoOwnership")
-			{
-				color = Color.Yellow;
-			}
-			else if (info.Relationship.ToString() == "Enemies")
-			{
-				color = Color.Red;
+				display.SetValue("FontColor", color);
+				display.WritePublicText(text);
+				display.ShowPublicTextOnScreen();
 			}
 
-			if (info.HitPosition.HasValue)
-			{
-				sb.AppendLine();
-				sb.Append($"Hit point: {info.HitPosition.Value.ToString("0.000")}");
-				sb.AppendLine();
-				sb.Append($"GPS:objectHit{(info.HitPosition.Value.ToString("0.000").Replace("{", string.Empty).Replace("}", string.Empty).Replace("X", string.Empty).Replace("Y", string.Empty).Replace("Z", string.Empty).Replace(" ", string.Empty))}:");
-			}
-
-			lcd.SetValue("FontColor", color);
-			lcd.WritePublicText(sb.ToString());
-
-			lcd.ShowPublicTextOnScreen();
-
-			lcd2.SetValue("FontColor", color);
-			lcd2.WritePublicText(sb.ToString());
-
-			lcd2.ShowPublicTextOnScreen();
 		}
 
-
-
-
-
-
-
-
-
+		//CUT HERE
 	}
+
 }
